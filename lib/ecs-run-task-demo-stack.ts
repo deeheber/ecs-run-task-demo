cf0137c7329883db854f6905036347ef57726d72
Danielle Heberling <deeheber@users.noreply.github.com>
2024-11-06 10:14:51 -0800

    Revise cdk init to match my conventions

--- lib/ecs-run-task-demo-stack.ts
+++ lib/ecs-run-task-demo-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 
-export class EcsRunTaskDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class EcsRunTaskDemoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsRunTaskDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
