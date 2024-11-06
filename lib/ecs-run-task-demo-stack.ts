680468bc7abf1f6613d0b4fc0c8b3318c2f41c36
Danielle Heberling <deeheber@users.noreply.github.com>
2024-11-06 10:01:46 -0800

    Init cdk project

---
+++ lib/ecs-run-task-demo-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class EcsRunTaskDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'EcsRunTaskDemoQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
